4c21a2bfd7b8df2da69e5988b08f70b577f77254
rightvalue <bluebird6922@gamil.com>
2021-06-07 09:33:14 +0900

    s

--- src/app/view/wip/wip-content/wip-content.component.ts
+++ src/app/view/wip/wip-content/wip-content.component.ts
@@ -1,43 +1,16 @@
 import { UserService } from '../../../service/user.service';
 
+
 import {AfterViewInit, Component, ViewChild} from '@angular/core';
 import {MatPaginator} from '@angular/material/paginator';
 import {MatSort} from '@angular/material/sort';
 import {MatTableDataSource} from '@angular/material/table';
 
-export interface WipData {
+export interface UserData {
   id: number;
-  equipmentId: number;
-  workShop: string;
-  technician: string;
-  customerId: number;
-  contactId: number;
-  notes: string;
-  wipstatus: string;
-  visitRequired: true;
-  breakDown: true;
-  engineHoursTillNow: number;
-  dateCompleted: string;
-  createdBy: string;
-  isArchive: true;
-  priority: number;
-  warranty: true;
-  location: string;
-  serviceType: string;
-  startDate: string;
-  contractor: string;
-  archiveDate: string;
-  archiveBy: string;
-  jobNo: number;
-  ownerFolder: true;
-  idplate: true;
-  promisedDate: string;
-  ssmaTimeStamp: string;
-  model: string;
-  serialNumber: string;
-  make: string;
-  customerName: string;
-contactName: string;
+  name: string;
+  progress: string;
+  fruit: string;
 }
 
 /** Constants used to fill up our data base. */
@@ -58,27 +31,19 @@ const NAMES: string[] = [
   templateUrl: 'wip-content.component.html',
 })
 export class WipContentComponent implements AfterViewInit {
-  loading = true;
   displayedColumns: string[] = ['id', 'name', 'progress', 'fruit'];
-  dataSource: MatTableDataSource<WipData>;
-  wips = [];
+  dataSource: MatTableDataSource<any>;
+
   @ViewChild(MatPaginator) paginator: MatPaginator;
   @ViewChild(MatSort) sort: MatSort;
 
   constructor(private userService: UserService) {
     // Create 100 users
-    this.userService.getRequest('/api/Wip/GetAllWip').subscribe(
-      res => {
-        this.loading = false;
-        this.dataSource = new MatTableDataSource(res['data']);
-      },
-      err => {
-        console.log(err)
-        this.loading = false;
-        this.userService.handleError(err)
-      }
-    );
+    const users = Array.from({length: 100}, (_, k) => createNewUser(k + 1));
 
+    // Assign the data to the data source for the table to render
+    this.dataSource = new MatTableDataSource(users);
+    console.log(this.dataSource);
   }
 
   ngAfterViewInit() {
@@ -97,14 +62,14 @@ export class WipContentComponent implements AfterViewInit {
 }
 
 /** Builds and returns a new User. */
-// function createNewUser(id: number): WipData {
-//   // const name = NAMES[Math.round(Math.random() * (NAMES.length - 1))] + ' ' +
-//   //   NAMES[Math.round(Math.random() * (NAMES.length - 1))].charAt(0) + '.';
+function createNewUser(id: number){
+  const name = NAMES[Math.round(Math.random() * (NAMES.length - 1))] + ' ' +
+    NAMES[Math.round(Math.random() * (NAMES.length - 1))].charAt(0) + '.';
 
-//   // return {
-//   //   id: id.toString(),
-//   //   name: name,
-//   //   progress: Math.round(Math.random() * 100).toString(),
-//   //   fruit: FRUITS[Math.round(Math.random() * (FRUITS.length - 1))]
-//   // };
-// }
+  return {
+    id: id,
+
+    progress: Math.round(Math.random() * 100).toString(),
+    fruit: FRUITS[Math.round(Math.random() * (FRUITS.length - 1))]
+  };
+}
